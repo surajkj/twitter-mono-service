bfcd43835aa76097bdffdc3218341479b60b752e
surajkj <srj121.kj@gmail.com>
2021-11-06 09:57:01 +0530

    refactor package

--- src/main/java/com/twitter/Twitter.java
+++ src/main/java/com/twitter/Twitter.java
@@ -1,13 +1,13 @@
-package com.twitter.twittermonoservice;
+package com.twitter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TwitterMonoServiceApplication {
+public class Twitter {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TwitterMonoServiceApplication.class, args);
+		SpringApplication.run(Twitter.class, args);
 	}
 
 }
