91fd17d8c7461a208222b83bef75b11e591fe1c3
surajkj <srj121.kj@gmail.com>
2021-11-06 09:37:14 +0530

    Added Spring files

---
+++ src/main/java/com/twitter/twittermonoservice/TwitterMonoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.twitter.twittermonoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterMonoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterMonoServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/twitter/twittermonoservice/TwitterMonoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twitter.twittermonoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterMonoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
