3110ec6070b61e011f92f09b95d9d1e14a292f7c
surajkj <srj121.kj@gmail.com>
2021-11-06 15:32:00 +0530

    Validation in progress

--- src/main/java/com/twitter/controller/UserController.java
+++ src/main/java/com/twitter/controller/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import javax.ws.rs.core.MediaType;
 
 @Slf4j
@@ -21,7 +22,7 @@ public class UserController {
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON)
     @Operation(description = "Create user", summary = "User will be created & OTP will be sent to email")
-    public void createUser(@JsonView(FrontendViews.CreateUserView.class) @RequestBody Users user){
+    public void createUser(@JsonView(FrontendViews.CreateUserView.class) @Valid @RequestBody Users user){
         log.info("Creating user {} ", user);
     }
 
