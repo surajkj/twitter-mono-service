c3d2073dfafc9aa62a3eaeba831eb6d4aba04cfe
surajkj <srj121.kj@gmail.com>
2021-11-06 13:39:43 +0530

    Random String generator

---
+++ src/main/java/com/twitter/utility/Utility.java
@@ -0,0 +1,32 @@
+package com.twitter.utility;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Utility {
+
+    public static String generateRandomString(int length){
+        String alphaNumericString = "ABCDEFGHIJKLMNPQRSTUVWXYZ"
+                + "123456789";
+        return generateRandomSalt(length, alphaNumericString);
+    }
+
+    public static String generateRandomNumber(int length){
+        String alphaNumericString = "123456789";
+        return generateRandomSalt(length, alphaNumericString);
+    }
+
+    private static String generateRandomSalt(int length,
+                                             String salt){
+        StringBuilder sb = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            // generate a random number between
+            // 0 to AlphaNumericString variable length
+            int index = (int)(salt.length() * Math.random());
+            // add Character one by one in end of sb
+            sb.append(salt.charAt(index));
+        }
+        return sb.toString();
+    }
+
+}
