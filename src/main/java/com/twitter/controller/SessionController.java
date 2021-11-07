1c0a83f295b43bc7d19b58c68b9c5ddb1d1b4728
surajkj <srj121.kj@gmail.com>
2021-11-07 11:48:17 +0530

    Create device Table

---
+++ src/main/java/com/twitter/controller/SessionController.java
@@ -0,0 +1,16 @@
+package com.twitter.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("api/v1/user")
+@Tag(name = "Session")
+public class SessionController {
+
+
+
+}
