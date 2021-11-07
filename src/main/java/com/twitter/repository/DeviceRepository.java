3443dfb66d9016a8ec3158cd82012c6e3635113a
surajkj <srj121.kj@gmail.com>
2021-11-07 12:08:50 +0530

    insert device

--- src/main/java/com/twitter/repository/DeviceRepository.java
+++ src/main/java/com/twitter/repository/DeviceRepository.java
@@ -1,4 +1,15 @@
 package com.twitter.repository;
 
+import com.twitter.dto.Device;
+import org.jdbi.v3.sqlobject.customizer.BindBean;
+import org.jdbi.v3.sqlobject.statement.GetGeneratedKeys;
+import org.jdbi.v3.sqlobject.statement.SqlUpdate;
+
 public interface DeviceRepository {
+
+    @SqlUpdate("insert into device (agent_type, agent_name, agent_producer, agent_version, os, os_producer, os_version, device_type, user_agent) " +
+            "values(:agentType, :agentName, :agentProducer, :agentVersion, :os, :osProducer, :osVersion, :deviceType, :userAgent);")
+    @GetGeneratedKeys
+    Long createDevice(@BindBean Device device);
+
 }
